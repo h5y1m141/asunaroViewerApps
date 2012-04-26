5fc1caecfe0a0e07e81e1faeb5bf89f0fb343864
hiroshi oyamada <h5y1m141+github@gmail.com>
2012-04-27 07:37:03 +0900

    現在選択中のブロガー情報を別画面で引き継ぐ必要あるのでコントローラとなる
    ものを作成

--- Resources/app.js
+++ Resources/app.js
@@ -4,24 +4,14 @@ myApps.ui = {};
 myApps.ui.util = require('ui/ui');
 myApps.ui.util.tableView = require('ui/tableView');
 myApps.ui.util.webView = require('ui/webView');
+myApps.ui.mainTable = myApps.ui.util.tableView.init('tableView');
 myApps.entries = require('model/entries');
+myApps.contoller = require('controller/main');
+
 
 (function(){
-
-  myApps.entries.load('oyamada',function(entries){
-    myApps.ui.mainTable = myApps.ui.util.tableView.init('tableView');
-    var rows = [];
-    for(var i=0;i<entries.length;i++){
-      var entry = entries[i];
-
-      var row = myApps.ui.util.createEntryRow(entry);
-      rows.push(row);
-      myApps.ui.mainTable.setData(rows,{
-        animated:true,
-        animationStyle:Titanium.UI.iPhone.RowAnimationStyle.DOWN
-      });
-    }
-  });
+  var blogger = 'oyamada';
+  myApps.contoller.main(blogger);
   myApps.ui.webView = myApps.ui.util.webView.init();
   myApps.ui.util.addElement(myApps.ui.mainTable);
 
---
+++ Resources/controller/main.js
@@ -0,0 +1,18 @@
+var exports = {
+  main:function(/* string */ blogger){
+    myApps.entries.load(blogger,function(entries){
+
+      var rows = [];
+      for(var i=0;i<entries.length;i++){
+        var entry = entries[i];
+
+        var row = myApps.ui.util.createEntryRow(entry);
+        rows.push(row);
+        myApps.ui.mainTable.setData(rows,{
+          animated:true,
+          animationStyle:Titanium.UI.iPhone.RowAnimationStyle.DOWN
+        });
+      }
+    });
+  }
+};
