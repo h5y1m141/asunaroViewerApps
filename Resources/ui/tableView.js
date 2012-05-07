22a0b4f4ac2768d1123c4a7a5c01d0b270b49581
hiroshi oyamada <h5y1m141+github@gmail.com>
2012-05-07 18:33:30 +0900

    エントリ一覧用のTableViewのみイベントリスナーを設定するようにtableView.jsを修正

--- Resources/ui/tableView.js
+++ Resources/ui/tableView.js
@@ -3,36 +3,31 @@ var $$ = require('ui/styles').prop;
 
 var exports = {
   init:function(option){
-    var tableViewStyle = option||tableView;
+    var tableViewStyle = option||'tableView';
     Ti.API.info(tableViewStyle);
     var tableView = Ti.UI.createTableView($$[tableViewStyle]);
+    // エントリ一覧用のTableViewを生成する際には過去のエントリ情報を
+    // 読み込む処理が必要になるためイベントリスナーを設定
+    if(tableViewStyle==='tableView'){
+      tableView.addEventListener('scrollEnd',function(e){
 
-    tableView.addEventListener('click',function(e){
-      // var index = e.index;
-      // var row = myApps.ui.createComposeRow();
-      // row.rowNum =  index;
-      // myApps.ui.mainTable.insertRowAfter(index,row,{animationStyle:Titanium.UI.iPhone.RowAnimationStyle.DOWN});
-      // row.addEventListener('click',function(e){
-      //   myApps.ui.mainTable.deleteRow(index+1,{animationStyle:Titanium.UI.iPhone.RowAnimationStyle.UP});
-      // });
+        var last_index = tableView.data[0].rows.length - 1;
+        var blogger = tableView.data[0].rows[last_index].data.blogger;
 
-    });
-    tableView.addEventListener('scrollEnd',function(e){
-      var last_index = tableView.data[0].rows.length - 1;
-      var blogger = tableView.data[0].rows[last_index].data.blogger;
+        myApps.entries.findLocalJSONDB(blogger,function(cached){
 
-      myApps.entries.findLocalJSONDB(blogger,function(cached){
+          for(var i=0;i<cached.length;i++){
+            var entry = cached[i];
+            Ti.API.info('読み込まれたエントリタイトル：'+cached[i].title);
+            var row = myApps.ui.util.createEntryRow(entry);
+            tableView.appendRow(row,{
+              animated:false
+            });
+          }
+        });
 
-        for(var i=0;i<cached.length;i++){
-          var entry = cached[i];
-          Ti.API.info('読み込まれたエントリタイトル：'+cached[i].title);
-          var row = myApps.ui.util.createEntryRow(entry);
-          tableView.appendRow(row,{
-            animated:false
-          });
-        }
       });
-    });
+    }
     return tableView;
   }
 };
