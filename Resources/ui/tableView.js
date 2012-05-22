ef84a5ee3b861aa29ea8a4edec958322a299c3a2
hiroshi oyamada <h5y1m141+github@gmail.com>
2012-05-23 07:38:03 +0900

    エントリ一覧の一番下に過去のエントリを読み込み処理を実装

--- Resources/ui/tableView.js
+++ Resources/ui/tableView.js
@@ -7,29 +7,19 @@ var exports = {
     var tableViewStyle = option||'tableView';
     Ti.API.info(tableViewStyle);
     var tableView = Ti.UI.createTableView($$[tableViewStyle]);
-    // エントリ一覧用のTableViewを生成する際には過去のエントリ情報を
-    // 読み込む処理が必要になるためイベントリスナーを設定
-    if(tableViewStyle==='tableView'){
-      tableView.addEventListener('scrollEnd',function(e){
 
-        var last_index = tableView.data[0].rows.length - 1;
-        var blogger = tableView.data[0].rows[last_index].data.blogger;
-
-        myApps.entries.findLocalJSONDB(blogger,function(cached){
-
-          for(var i=0;i<cached.length;i++){
-            var entry = cached[i];
-            Ti.API.info('読み込まれたエントリタイトル：'+cached[i].title);
-            var row = self.createEntryRow(entry);
-            tableView.appendRow(row,{
-              animated:false
-            });
-          }
-        });
-
-      });
-    }
     return tableView;
+  },
+  createLoadOlderEntryRow:function(){
+    var row = Ti.UI.createTableViewRow($$.loadOlderEntryRow);
+    var label = Ti.UI.createLabel($$.reloadLabel);
+    label.text= '過去のエントリを読み込む';
+    row.add(label);
+    row.addEventListener('click',function(e){
+      myApps.ui.actInd.show();
+      myApps.contoller.oldEntries();
+    });
+    return row;
   },
   createBloggerRow:function(/* json */ blogger){
 
