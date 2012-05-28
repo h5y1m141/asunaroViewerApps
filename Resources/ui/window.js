6c0fcf3f7d878764d555b597f5ca0e7a433e0828
hiroshi oyamada <h5y1m141+github@gmail.com>
2012-05-29 07:52:41 +0900

    WebView用のWindow生成するメソッドを準備

--- Resources/ui/window.js
+++ Resources/ui/window.js
@@ -3,41 +3,14 @@ var $$ = require('ui/styles').prop;
 var exports = {
   createMainWindow :function(){
     var win = Titanium.UI.createWindow($$.win);
-    win.title = 'あすなろBLOG';
-    win.rightNavButton = (function(){
-      var button = Titanium.UI.createButton($$.refreshBtn);
-      button.addEventListener('click', function() {
-        myApps.ui.actInd.show();
-        myApps.contoller.updateEntries();
-      });
-      return button;
-    })();
 
-    win.leftNavButton = (function(){
-      var button = Titanium.UI.createButton($$.listBtn);
-      button.addEventListener('click', function() {
-
-        if(!myApps.ui.mainTable.moved){
-          myApps.ui.mainTable.animate({
-            duration:180,
-            left:200
-          });
-          myApps.ui.mainTable.moved = true;
-          myApps.ui.webView.hide();
-        }else{
-          myApps.ui.mainTable.animate({
-            duration:180,
-            left:5
-          });
-          myApps.ui.mainTable.moved = false;
-          myApps.ui.webView.hide();
-        }
-      });
-      return button;
-    })();
     return win;
   },
   createEntryWindow :function(){
+    var win = Titanium.UI.createWindow($$.win);
+    return win;
+  },
+  createEntryDetailWindow :function(){
     var win = Titanium.UI.createWindow($$.win);
     return win;
   }
